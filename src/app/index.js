37c4c44091f6eeccf02cb852231167563f23c530
William Yaworsky <will.yaworsky@gmail.com>
2015-03-28 01:42:45 -0400

    working but sluggish

--- src/app/index.js
+++ src/app/index.js
@@ -1,7 +1,7 @@
 var canvas = document.getElementById('canvas');
 
-canvas.width = window.innerWidth;
-canvas.height = window.innerHeight;
+canvas.width = window.innerWidth - 100;
+canvas.height = window.innerHeight - 400;
 
 var context2 = canvas.getContext('2d');
 var context  = new C2S(500, 500);
@@ -9,23 +9,24 @@ var centerX  = canvas.width / 2;
 var centerY  = canvas.height / 2;
 var radius   = 70;
 
-// context.beginPath();
-// context.arc(centerX, centerY, radius, 0, 2 * Math.PI, false);
-// context.fillStyle = 'black';
-// context.fill();
-// context.lineWidth = 5;
-// context.strokeStyle = '#000';
-// context.stroke();
+var doStuffWithContexts = function() {
+  if (arguments[0] === 'set') {
+    context[arguments[0]] = arguments[1];
+    context2[arguments[0]] = arguments[1];
+  } else {
+    var sliced = Array.prototype.slice.call(arguments, 1);
+    context[arguments[0]].apply(context, sliced);
+    context2[arguments[0]].apply(context2, sliced);
+  }
+};
 
-// context2.beginPath();
-// context2.arc(centerX, centerY, radius, 0, 2 * Math.PI, false);
-// context2.fillStyle = 'black';
-// context2.fill();
-// context2.lineWidth = 5;
-// context2.strokeStyle = '#000';
-// context2.stroke();
-
-var res = context.getSerializedSvg();
+var updateTextArea = function() {
+  var res = context.getSerializedSvg();
+  res = '<?xml version="1.0" encoding="utf-8" standalone="yes"?>\n' +
+        '<!DOCTYPE svg PUBLIC "-//W3C//DTD SVG 1.1//EN" "http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd">\n' +
+        res;
+  $('#value').val(res);
+};
 
 var actx = new (window.AudioContext || window.webkitAudioContext)();
 navigator.webkitGetUserMedia({audio:true}, function(stream) {
@@ -43,8 +44,9 @@ navigator.webkitGetUserMedia({audio:true}, function(stream) {
   var WIDTH  = canvas.width;
   var HEIGHT = canvas.height;
 
+  doStuffWithContexts('set', 'fillStyle', 'black');
   requestAnimationFrame(function loop() {
-    context2.clearRect(0, 0, canvas.width, canvas.height);
+    doStuffWithContexts('clearRect', 0, 0, canvas.width, canvas.height);
     var times = new Uint8Array(analyser.frequencyBinCount);
     analyser.getByteTimeDomainData(times);
     for (var i = 0; i < times.length; i++) {
@@ -53,23 +55,12 @@ navigator.webkitGetUserMedia({audio:true}, function(stream) {
       var h        = HEIGHT * percent;
       var offset   = HEIGHT - h - 1;
       var barWidth = WIDTH / times.length;
-      context2.fillStyle = 'black';
-      context2.fillRect(i * barWidth, offset, 1, 5 );
+      doStuffWithContexts('fillRect', i * barWidth, offset, 1, 5 );
     }
+    updateTextArea();
     requestAnimationFrame(loop);
   });
 
 }, function() {
   alert('bad browser');
 });
-
-setTimeout(function() {
-  res = '<?xml version="1.0" encoding="utf-8" standalone="yes"?>\n' +
-        '<!DOCTYPE svg PUBLIC "-//W3C//DTD SVG 1.1//EN" "http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd">\n' +
-        res;
-  $('#value').val(res);
-})
-
-console.log($('#value').val())
-
-console.log(res);
