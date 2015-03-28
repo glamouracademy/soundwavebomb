4ebbae807c0f7cea3a71fa364eb85989813e7c39
Charlotte Chang <yellowspiderlove@gmail.com>
2015-03-28 18:15:15 -0400

    added rotation js

---
+++ public/assets/js/rotation.js
@@ -0,0 +1,42 @@
+var howOften = 2; //number often in seconds to rotate
+var current = 0; //start the counter at 0
+var ns6 = document.getElementById&&!document.all; //detect netscape 6
+
+// place your images, text, etc in the array elements here
+var items = new Array();
+    items=["Sticks and stones may break my bones but now your words can hurt me",
+    "Put your money where your mouth is",
+    "Give your friend a fart",
+    "THE ONLY physical manifestation of sound",
+    "Helping millions still not be able to express themselves",
+    "Printers can print sound now",
+    "Its like sheet music but completely different",
+    "There may be some lossiness",
+    "A revolutionary way to create your very own tramp stamp designs",
+    "Its not FLAC",
+    "Better sound quality than most Idaho potatoes",
+    "Say it with style",
+    "Style it with say",
+    "Give your sweetheart a fart and tell them it says something sweet",
+    "Sound has never looked so good",
+    "Its over nine thousand"]
+
+function rotater() {
+    document.getElementById("placeholder").innerHTML = items[current];
+    current = (current==items.length-1) ? 0 : current + 1;
+    setTimeout("rotater()",howOften*1000);
+}
+
+// function rotater() {
+//     if(document.layers) {
+//         document.placeholderlayer.document.write(items[current]);
+//         document.placeholderlayer.document.close();
+//     }
+//     if(ns6)document.getElementById("placeholderdiv").innerHTML=items[current]
+//         if(document.all)
+//             placeholderdiv.innerHTML=items[current];
+
+//     current = (current==items.length-1) ? 0 : current + 1; //increment or reset
+//     setTimeout("rotater()",howOften*1000);
+// }
+window.onload=rotater;
