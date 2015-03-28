c582dc765fb1bb0afb46e4deaaf976683cf91dd9
William Yaworsky <will.yaworsky@gmail.com>
2015-03-28 19:29:57 -0400

    added tag lines

--- public/assets/js/rotation.js
+++ public/assets/js/rotation.js
@@ -4,22 +4,30 @@ var ns6 = document.getElementById&&!document.all; //detect netscape 6
 
 // place your images, text, etc in the array elements here
 var items = new Array();
-    items=["Sticks and stones may break my bones but now your words can hurt me",
-    "Put your money where your mouth is",
-    "Give your friend a fart",
-    "THE ONLY physical manifestation of sound",
-    "Helping millions still not be able to express themselves",
-    "Printers can print sound now",
-    "Its like sheet music but completely different",
-    "There may be some lossiness",
-    "A revolutionary way to create your very own tramp stamp designs",
-    "Its not FLAC",
-    "Better sound quality than most Idaho potatoes",
-    "Say it with style",
-    "Style it with say",
-    "Give your sweetheart a fart and tell them it says something sweet",
-    "Sound has never looked so good",
-    "Its over nine thousand"]
+    items=[
+        "Sticks and stones may break my bones but now your words can hurt me",
+        "If a 2D picture is worth one thousand words then a 3D picture has got to be worth like way more.  Maybe like a million words?",
+        "Put your money where your mouth is",
+        "Give your friend a fart",
+        "THE ONLY physical manifestation of sound",
+        "Helping millions still not be able to express themselves",
+        "Printers can print sound now",
+        "The things say you can not be undone",
+        "Sorry we didn't buy the domain",
+        "Its like sheet music but completely different",
+        "There may be some lossiness",
+        "Mince your words",
+        "Horse powered",
+        "A revolutionary way to create your very own tramp stamp designs",
+        "Its not FLAC",
+        "Better sound quality than most Idaho potatoes",
+        "Say it with style",
+        "Style it with say",
+        "Give your sweetheart a fart and tell them it says something sweet",
+        "Sound has never looked so good",
+        "Its over nine thousand",
+        "Ambrosia for your thoughts"
+    ];
 
 function rotater() {
     document.getElementById("placeholder").innerHTML = items[current];
