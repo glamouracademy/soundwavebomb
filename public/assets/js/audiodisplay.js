7b72c3f904692d0f7a3d89632a4224927ea33073
William Yaworsky <will.yaworsky@gmail.com>
2015-03-28 14:36:29 -0400

    added multi drawers

--- public/assets/js/audiodisplay.js
+++ public/assets/js/audiodisplay.js
@@ -3,7 +3,88 @@ var spacing  = 1.2;  // spacing factor between bars
 var fraction = 4;   // fraction of width for connecting bars
 var heightOfMiddleLine = 10;
 
+var getSvg = function(svgc) {
+    return '<?xml version="1.0" encoding="utf-8" standalone="yes"?>\n' +
+    '<!DOCTYPE svg PUBLIC "-//W3C//DTD SVG 1.1//EN" "http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd">\n' +
+    svgc.getSerializedSvg();
+};
+
+var updateInput = function(svgc) {
+    setTimeout(function() {
+        $('#val').val(getSvg(svgc));
+    });
+};
+
 var drawers = {
+    'wave': function(width, height, context, data) {
+        var svgc = new C2S(width, height);
+        var step = Math.ceil(data.length / width);
+        var amp  = height / 2;
+
+        context.fillStyle = "#FFFFFF";
+        svgc.fillStyle    = "#FFFFFF";
+
+        context.clearRect(0, 0, width, height);
+
+        context.beginPath();
+        svgc.beginPath();
+
+        context.moveTo(0, height / 2);
+        svgc.moveTo(0, height / 2);
+
+        for(var i = 0; i < width; i += inc * spacing){
+            var min = 1.0;
+            var max = -1.0;
+            for (j=0; j<step; j++) {
+                var datum = data[(i*step)+j];
+                if (datum < min)
+                    min = datum;
+                if (datum > max)
+                    max = datum;
+            }
+            var x = i;
+            var y = (1 + min) * amp;
+            var w = inc;
+            var h = (Math.max(1, (max - min) * amp));
+
+            context.lineTo(x, y);
+            svgc.lineTo(x, y);
+        }
+
+        context.lineTo(width, height / 2);
+        svgc.lineTo(width, height / 2);
+
+        for(var i = width; i > 0; i -= inc * spacing){
+            var min = 1.0;
+            var max = -1.0;
+            for (j=0; j<step; j++) {
+                var datum = data[(i*step)+j];
+                if (datum < min)
+                    min = datum;
+                if (datum > max)
+                    max = datum;
+            }
+            var x = i;
+            var y = (1 + min) * amp;
+            var w = inc;
+            var h = (Math.max(1, (max - min) * amp));
+
+            context.lineTo(x, y + h);
+            svgc.lineTo(x, y + h);
+        }
+
+        context.closePath();
+        svgc.closePath();
+
+        context.fill();
+        svgc.fill();
+
+        context.fillRect(0, height / 2 - (heightOfMiddleLine / 2), width, heightOfMiddleLine);
+        svgc.fillRect(0, height / 2 - (heightOfMiddleLine / 2), width, heightOfMiddleLine);
+
+        updateInput(svgc);
+    },
+
     'default': function(width, height, context, data) {
         var svgc = new C2S(width, height);
         var step = Math.ceil(data.length / width);
@@ -39,17 +120,10 @@ var drawers = {
         context.fillRect(0, height / 2 - (heightOfMiddleLine / 2), width, heightOfMiddleLine);
         svgc.fillRect(0, height / 2 - (heightOfMiddleLine / 2), width, heightOfMiddleLine);
 
-        var Q =
-            '<?xml version="1.0" encoding="utf-8" standalone="yes"?>\n' +
-            '<!DOCTYPE svg PUBLIC "-//W3C//DTD SVG 1.1//EN" "http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd">\n' +
-            svgc.getSerializedSvg();
-
-        setTimeout(function() {
-            $('#val').val(Q);
-        }, 100);
+        updateInput(svgc);
     }
 };
 
 function drawBuffer( width, height, context, data ) {
-    drawers[$('#drawer').val()](width, height, context, data);
+    drawers[$('#drawer').val() || 'default'](width, height, context, data);
 }
